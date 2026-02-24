73b4e1fa05b49d951ae9f8361f9f7c574440c697
unknown <mk1323@srmist.edu.in>
2026-02-24 18:10:48 +0530

    Added uc3 changes

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,17 @@
 public class OOPSBannerApp{
      public static void main(String[] args) {
 
-        System.out.println("  **  " + "  **  " + " **  " + "  ***  ");
-        System.out.println(" *     * " + " *     * " + " *     * " + " *     * ");
-        System.out.println("*       " + "       " + " *     * " + "        ");
-        System.out.println("*       " + "       " + " ***  " + "  ***  ");
-        System.out.println("*       " + "       *" + " *       " + "       * ");
-        System.out.println(" *     * " + " *     * " + " *       " + " *     * ");
-        System.out.println("  **  " + "  **  " + " *       " + "  ***  ");
+        System.out.println(String.join("\n",
+
+                String.join("", "  **  ", "  **  ", " **  ", "  ***  "),
+                String.join("", " *     * ", " *     * ", " *     * ", " *     * "),
+                String.join("", "*       ", "       ", " *     * ", "        "),
+                String.join("", "*       ", "       ", " ***  ", "  ***  "),
+                String.join("", "*       ", "       *", " *       ", "       * "),
+                String.join("", " *     * ", " *     * ", " *       ", " *     * "),
+                String.join("", "  **  ", "  **  ", " *       ", "  ***  ")
+
+        ));
     }
+
     }
